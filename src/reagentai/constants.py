046010c61854a84a246a9ae5351b3ddee81a9a8b
Kowalski1024 <tomasz_wojnar@hotmail.com>
2025-05-18 19:10:53 +0200

    chore: update INSTRUCTIONS_PATH and define AVAILABLE_LLM_MODELS

--- src/reagentai/constants.py
+++ src/reagentai/constants.py
@@ -1,1 +1,5 @@
-INSTRUCTIONS_PATH: str = "src/reagentai/instructions/instructions.txt"
+INSTRUCTIONS_PATH: str = "src/reagentai/llm/instructions/instructions.txt"
+AVAILABLE_LLM_MODELS = [
+    "gemini-2.0-flash",
+    "gemini-2.5-flash-preview-04-17",
+]
