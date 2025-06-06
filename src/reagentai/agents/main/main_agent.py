b9b10ce336aea534638a0337b7f13e65a84a7a2d
Kowalski1024 <tomasz_wojnar@hotmail.com>
2025-06-06 21:04:17 +0200

    [feat] implement create_tool helper function and node parsing logic

--- src/reagentai/agents/main/main_agent.py
+++ src/reagentai/agents/main/main_agent.py
@@ -1,6 +1,7 @@
 from pydantic_ai import Tool
 
 from src.reagentai.common.client import LLMClient
+from src.reagentai.tools.helpers import create_tool
 from src.reagentai.tools.retrosynthesis import perform_retrosynthesis
 from src.reagentai.tools.smiles import is_valid_smiles
 
@@ -19,7 +20,7 @@ def create_main_agent() -> LLMClient:
     with open(MAIN_AGENT_INSTRUCTIONS_PATH) as instructions_file:
         instructions = instructions_file.read()
 
-    tools = [Tool(perform_retrosynthesis), Tool(is_valid_smiles)]
+    tools = map(create_tool, [perform_retrosynthesis, is_valid_smiles])
 
     llm_client = LLMClient(model_name=MAIN_AGENT_MODEL, tools=tools, instructions=instructions)
 
